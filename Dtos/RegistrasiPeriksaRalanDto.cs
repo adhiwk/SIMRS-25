c5de253fdfe099db408a7cbada7109e6e23dde1d
adhiwk <info@indovb.com>
2025-06-03 20:25:41 +0800

    create http service for backend request

--- Dtos/RegistrasiPeriksaRalanDto.cs
+++ Dtos/RegistrasiPeriksaRalanDto.cs
@@ -8,22 +8,22 @@ namespace SIMRSKB.Dto
 {
     public class RegistrasiPeriksaRalanDto
     {
-        public string tgl_registrasi { get; set; }
-        public string no_reg { get; set; }
-        public string no_rawat { get; set; }
-        public string no_rkm_medis { get; set; }
-        public string nm_pasien { get; set; }
+        public string? tgl_registrasi { get; set; }
+        public string? no_reg { get; set; }
+        public string? no_rawat { get; set; }
+        public string? no_rkm_medis { get; set; }
+        public string? nm_pasien { get; set; }
         public string alamat { get; set; }
-        public string jk { get; set; }
-        public string tgl_lahir { get; set; }
-        public string nm_dokter { get; set; }
-        public string kd_dokter { get; set; }
+        public string? jk { get; set; }
+        public string? tgl_lahir { get; set; }
+        public string? nm_dokter { get; set; }
+        public string? kd_dokter { get; set; }
         public string nm_poli { get; set; }
-        public string kd_poli { get; set; }
-        public string png_jawab { get; set; }
-        public string kd_pj { get; set; }
-        public string no_peserta { get; set; }
-        public string stts_daftar { get; set; }
-        public string stts { get; set; }
+        public string? kd_poli { get; set; }
+        public string? png_jawab { get; set; }
+        public string? kd_pj { get; set; }
+        public string? no_peserta { get; set; }
+        public string? stts_daftar { get; set; }
+        public string? stts { get; set; }
     }
 }
---
+++ Http/HttpServices.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+namespace SIMRS25.Http
+{
+    public class HttpServices
+    {
+        private readonly HttpClient _httpClient;
+
+        public string? AuthToken { get; set; } // Sanctum token
+
+        public HttpServices(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        private void AddAuthorizationHeaderIfNeeded()
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = null;
+
+            if (!string.IsNullOrWhiteSpace(AuthToken))
+            {
+                _httpClient.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", AuthToken);
+            }
+        }
+
+        public async Task<TResponse?> GetAsync<TResponse>(string url, bool withAuth = true)
+        {
+            if (withAuth) AddAuthorizationHeaderIfNeeded();
+
+            var response = await _httpClient.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<TResponse>();
+        }
+
+        public async Task<TResponse?> PostAsync<TRequest, TResponse>(string url, TRequest data, bool withAuth = true)
+        {
+            if (withAuth) AddAuthorizationHeaderIfNeeded();
+
+            var response = await _httpClient.PostAsJsonAsync(url, data);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<TResponse>();
+        }
+
+        public async Task<TResponse?> PutAsync<TRequest, TResponse>(string url, TRequest data, bool withAuth = true)
+        {
+            if (withAuth) AddAuthorizationHeaderIfNeeded();
+
+            var response = await _httpClient.PutAsJsonAsync(url, data);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<TResponse>();
+        }
+
+        public async Task<TResponse?> DeleteAsync<TResponse>(string url, bool withAuth = true)
+        {
+            if (withAuth) AddAuthorizationHeaderIfNeeded();
+
+            var response = await _httpClient.DeleteAsync(url);
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadFromJsonAsync<TResponse>();
+        }
+    }
+}
